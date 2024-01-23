874b184f10a6c97e6ef0ba0ae3d8a4ff79f5b77f
JamesOlaleye <olanipekunjamesolaleye@gmail.com>
2024-01-23 03:53:03 +0100

    Backend completed successfully

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'https://pinvent-app-smoky.vercel.app/'],
+    origin: ['http://localhost:5173', 'https://pinvent-app-smoky.vercel.app'],
     credentials: true,
   })
 );
